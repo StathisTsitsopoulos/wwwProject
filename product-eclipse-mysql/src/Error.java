9e73d6582e963762f66422cf62d3af5449432427
StathisTsitsopoulos <56602210+StathisTsitsopoulos@users.noreply.github.com>
2020-11-06 23:33:54 +0200

    Add files via upload

---
+++ product-eclipse-mysql/src/Error.java
@@ -0,0 +1,36 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Error
+ */
+@WebServlet("/Error")
+public class Error extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.print("<a href='index.html'>Home</a>");
+		out.print("\nERROR this product's barcode already exists!");
+		
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
